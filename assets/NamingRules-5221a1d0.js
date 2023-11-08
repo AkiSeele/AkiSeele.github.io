fe497942791eaaebf15993e108ccd4ad84245933
AkiSeele <AkiSeele@users.noreply.github.com>
2023-11-08 09:21:49 +0000

    deploy: 53317651724783a5ae004850478b9e45095d5bb0

---
+++ assets/NamingRules-5221a1d0.js
@@ -0,0 +1,66 @@
+import{_ as d}from"./ValaxyMain.vue_vue_type_style_index_0_lang-ea91142e.js";import{_ as r,p as b,c as u,w as e,o as h,a as s,d as l,b as t,r as n,e as y}from"./app-5d79f60a.js";import"./YunFooter.vue_vue_type_script_setup_true_lang-c3b4d135.js";import"./YunCard.vue_vue_type_style_index_0_lang-71291467.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-d1903814.js";const E=JSON.parse('{"title":"命名习惯规则","description":"","frontmatter":{"title":"命名习惯规则","date":"2023-07-06T16:00:55.000Z","updated":"2023-07-06T16:21:06.000Z","tags":["笔记"],"categories":["笔记"]},"headers":[{"level":3,"title":"方法名","slug":"方法名","link":"#方法名","children":[]},{"level":3,"title":"文件名","slug":"文件名","link":"#文件名","children":[]},{"level":3,"title":"类名","slug":"类名","link":"#类名","children":[]},{"level":3,"title":"含义参考","slug":"含义参考","link":"#含义参考","children":[]}],"relativePath":"pages/posts/NamingRules.md","path":"/home/runner/work/AkiSeele.github.io/AkiSeele.github.io/pages/posts/NamingRules.md","lastUpdated":1699435261000}'),c=JSON.parse('{"title":"命名习惯规则","description":"","frontmatter":{"title":"命名习惯规则","date":"2023-07-06T16:00:55.000Z","updated":"2023-07-06T16:21:06.000Z","tags":["笔记"],"categories":["笔记"]},"headers":[{"level":3,"title":"方法名","slug":"方法名","link":"#方法名","children":[]},{"level":3,"title":"文件名","slug":"文件名","link":"#文件名","children":[]},{"level":3,"title":"类名","slug":"类名","link":"#类名","children":[]},{"level":3,"title":"含义参考","slug":"含义参考","link":"#含义参考","children":[]}],"relativePath":"pages/posts/NamingRules.md","path":"/home/runner/work/AkiSeele.github.io/AkiSeele.github.io/pages/posts/NamingRules.md","lastUpdated":1699435261000}'),m={name:"pages/posts/NamingRules.md",data(){return{data:c,frontmatter:c.frontmatter}},setup(){b("pageData",c)}},g={id:"方法名",tabindex:"-1"},_=s("blockquote",null,[s("p",null,[l("初始化 "),s("code",null,"AkiGive()")]),s("p",null,[l("表单提交 "),s("code",null,"onSubmit()")]),s("p",null,[l("新增 "),s("code",null,"addFunc()")]),s("p",null,[l("删除 "),s("code",null,"deleteFunc()")]),s("p",null,[l("编辑 "),s("code",null,"editFunc()")]),s("p",null,[l("重置 "),s("code",null,"Rewrite()")]),s("p",null,[l("导出 "),s("code",null,"onExport")]),s("p",null,[l("组件函数 "),s("code",null,"on + 组件Props名()")]),s("p",null,[l("工具类 "),s("code",null,"含义 + Utils()")])],-1),f={id:"文件名",tabindex:"-1"},v=s("blockquote",null,[s("p",null,[l("组件命名 "),s("code",null,"含义（驼峰）+ Aki.vue")]),s("p",null,[l("页面命名 "),s("code",null,"含义（驼峰，每个单词省略形式，不超过6位）")]),s("p",null,"路由命名"),s("blockquote",null,[s("p",null,[l("path: "),s("code",null,"/同文件名")]),s("p",null,[l("name: "),s("code",null,"/同文件名(首字母大写)")])])],-1),k={id:"类名",tabindex:"-1"},$=s("blockquote",null,[s("p",null,[s("code",null,"css"),l(" 类命名 层级间以_链接，小写 "),s("code",null,"含义_含义_含义")]),s("p",null,[l("最多拼接 3 个，超过以-重新拼接 "),s("code",null,"含义-含义-含义")])],-1),N={id:"含义参考",tabindex:"-1"},w=s("ul",null,[s("li",null,"布局")],-1),x=s("div",{class:"language-"},[s("span",{class:"copy"}),s("pre",{class:"shiki material-theme-palenight",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"头部    header(hd)")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"主体    body")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"尾部    footer(ft)")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"内容    main")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"侧栏    side")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"容器    box")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}})])])])],-1),S=s("ul",null,[s("li",null,"通用部件")],-1),A=s("div",{class:"language-"},[s("span",{class:"copy"}),s("pre",{class:"shiki material-theme-palenight",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"列表    list")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"列表项  item")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"表格    table")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"表单    form")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"链接    link")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"标题    caption/heading/title")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"菜单    menu")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"集合    group")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"条      bar")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"内容    content")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"结果    result")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}})])])])],-1),R=s("ul",null,[s("li",null,"组件")],-1),T=s("div",{class:"language-"},[s("span",{class:"copy"}),s("pre",{class:"shiki material-theme-palenight",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"按钮        btn")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"字体        text")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"下拉菜单    dropdown")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"工具栏      toolbar")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"分页        page")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"缩略图      thumbnail")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"警告框      alert")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"进度条      progress")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"导航条      navbar")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"导航        nav")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"子导航      subnav")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"面包屑      breadcrumb(crumb)")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"标签        label")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"徽章        badge")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"巨幕        jumbotron")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"面板        panel")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"洼地        well")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"标签页      tab")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"提示框      tooltip")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"弹出框      popover")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"轮播图      carousel")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"手风琴      collapse")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"定位浮标    affix")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}})])])])],-1),q=s("ul",null,[s("li",null,"状态")],-1),V=s("div",{class:"language-"},[s("span",{class:"copy"}),s("pre",{class:"shiki material-theme-palenight",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"前一个    prev")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"后一个    next")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"当前的    current")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}})]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"显示的    show")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"隐藏的    hide")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"打开的    open")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"关闭的    close")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}})]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"选中的    selected")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"有效的    active")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"默认的    default")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"反转的    toggle")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}})]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"禁用的    disabled")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"危险的    danger")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"主要的    primary")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"成功的    success")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"提醒的    info")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"警告的    warning")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"出错的    error")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}})]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"大型的    lg")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"小型的    sm")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}},"超小的    xs")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#babed8"}})])])])],-1);function Z(a,B,F,P,i,U){const o=y,p=d;return h(),u(p,{frontmatter:i.frontmatter,data:i.data},{"main-content-md":e(()=>[s("h3",g,[l("方法名 "),t(o,{class:"header-anchor",href:"#方法名","aria-hidden":"true"},{default:e(()=>[l("#")]),_:1})]),_,s("h3",f,[l("文件名 "),t(o,{class:"header-anchor",href:"#文件名","aria-hidden":"true"},{default:e(()=>[l("#")]),_:1})]),v,s("h3",k,[l("类名 "),t(o,{class:"header-anchor",href:"#类名","aria-hidden":"true"},{default:e(()=>[l("#")]),_:1})]),$,s("h3",N,[l("含义参考 "),t(o,{class:"header-anchor",href:"#含义参考","aria-hidden":"true"},{default:e(()=>[l("#")]),_:1})]),w,x,S,A,R,T,q,V]),"main-header":e(()=>[n(a.$slots,"main-header")]),"main-header-after":e(()=>[n(a.$slots,"main-header-after")]),"main-nav":e(()=>[n(a.$slots,"main-nav")]),"main-content":e(()=>[n(a.$slots,"main-content")]),"main-content-after":e(()=>[n(a.$slots,"main-content-after")]),"main-nav-before":e(()=>[n(a.$slots,"main-nav-before")]),"main-nav-after":e(()=>[n(a.$slots,"main-nav-after")]),comment:e(()=>[n(a.$slots,"comment")]),footer:e(()=>[n(a.$slots,"footer")]),aside:e(()=>[n(a.$slots,"aside")]),"aside-custom":e(()=>[n(a.$slots,"aside-custom")]),default:e(()=>[n(a.$slots,"default")]),_:3},8,["frontmatter","data"])}const G=r(m,[["render",Z]]);export{E as __pageData,G as default};
